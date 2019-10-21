858ef076aa3f948d352fbaab2b281fd4ac69e3e0
Sandeep Modak <sndp.mdk@gmail.com>
2019-10-21 18:47:59 +0530

    footer completed

--- js/script.js
+++ js/script.js
@@ -170,48 +170,48 @@ $(function () {
 /* =========================================
               Google Map
 ============================================ */
-//$(window).on('load', function () {
-//
-//    // Map Variables
-//    var addressString = '230 Broadway, NY, New York 10007, USA';
-//    var myLatlng = {
-//        lat: 40.712685,
-//        lng: -74.005920
-//    };
-//
-//    // 1. Render Map
-//    var map = new google.maps.Map(document.getElementById('map'), {
-//        zoom: 11,
-//        center: myLatlng
-//    });
-//
-//    // 2. Add Marker
-//    var marker = new google.maps.Marker({
-//        position: myLatlng,
-//        map: map,
-//        title: "Click To See Address"
-//    });
-//
-//    // 3. Add Info Window
-//    var infowindow = new google.maps.InfoWindow({
-//        content: addressString
-//    });
-//
-//    // Show info window when user clicks marker
-//    marker.addListener('click', function () {
-//        infowindow.open(map, marker);
-//    });
-//
-//    // 4. Resize Function
-//    google.maps.event.addDomListener(window, 'resize', function () {
-//
-//        var center = map.getCenter();
-//        google.maps.event.trigger(map, 'resize');
-//        map.setCenter(center);
-//
-//    });
-//
-//});
+$(window).on('load', function () {
+
+    // Map Variables
+    var addressString = '230 Broadway, NY, New York 10007, USA';
+    var myLatlng = {
+        lat: 40.712685,
+        lng: -74.005920
+    };
+
+    // 1. Render Map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: myLatlng
+    });
+
+    // 2. Add Marker
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "Click To See Address"
+    });
+
+    // 3. Add Info Window
+    var infowindow = new google.maps.InfoWindow({
+        content: addressString
+    });
+
+    // Show info window when user clicks marker
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+    // 4. Resize Function
+    google.maps.event.addDomListener(window, 'resize', function () {
+
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+
+    });
+
+});
 
 
 /* =========================================
